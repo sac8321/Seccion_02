439a92385faa53bd86d52bd0794f782dbb2ab8da
sac8321 <sac8321@gmail.com>
2017-10-18 16:01:04 -0600

    Sección 5 Clase 48

--- app/src/main/java/com/example/proyecto_pc/seccion_02/MainActivity.java
+++ app/src/main/java/com/example/proyecto_pc/seccion_02/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.proyecto_pc.seccion_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        listView =(ListView) findViewById(R.id.listView);
+
+        //Datos a mostrar
+        List<String> names = new ArrayList<String>();
+        names.add("Jorge");
+        names.add("Monica");
+        names.add("Andres");
+        names.add("Juan");
+
+        //Adaptador , la forma visual de como se muestran los datos
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
+
+        //Establecemos el adaptados con nuestro List view
+        listView.setAdapter(adapter);
+
 
     }
 }
