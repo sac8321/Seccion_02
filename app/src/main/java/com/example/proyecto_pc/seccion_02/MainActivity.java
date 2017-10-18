e788f97a076695a65cd234b83d069db030e80ab0
sac8321 <sac8321@gmail.com>
2017-10-18 15:17:41 -0600

    se agrega variable de list view

--- app/src/main/java/com/example/proyecto_pc/seccion_02/MainActivity.java
+++ app/src/main/java/com/example/proyecto_pc/seccion_02/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.proyecto_pc.seccion_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
     }
 }
