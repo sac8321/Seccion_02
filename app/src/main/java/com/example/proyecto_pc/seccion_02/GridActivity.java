3840185fb07171e59c7ff4109926e74d6243eb00
Seccion_02 <sac8321@gmail.com>
2017-11-23 21:41:44 -0600

    Seccion 5 Clase 54 Ingrese de boton que permite poder crear N valores dentro del gridview

--- app/src/main/java/com/example/proyecto_pc/seccion_02/GridActivity.java
+++ app/src/main/java/com/example/proyecto_pc/seccion_02/GridActivity.java
@@ -2,11 +2,16 @@ package com.example.proyecto_pc.seccion_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import org.w3c.dom.ProcessingInstruction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +20,10 @@ public class GridActivity extends AppCompatActivity {
 
     private List<String> names;
     private GridView gridView ;
+
+    private MyAdapter myAdapter;
+
+    private int counter =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +34,6 @@ public class GridActivity extends AppCompatActivity {
 
         //Datos a mostrar
         names = new ArrayList<String>();
-        names.add("Jorge");
-        names.add("Monica");
-        names.add("Andres");
-        names.add("Juan");
-        names.add("Jorge");
-        names.add("Monica");
-        names.add("Andres");
-        names.add("Juan");
-        names.add("Jorge");
-        names.add("Monica");
-        names.add("Andres");
-        names.add("Juan");
-        names.add("Jorge");
-        names.add("Monica");
-        names.add("Andres");
-        names.add("Juan");
-        names.add("Jorge");
-        names.add("Monica");
-        names.add("Andres");
-        names.add("Juan");
         names.add("Jorge");
         names.add("Monica");
         names.add("Andres");
@@ -60,8 +49,36 @@ public class GridActivity extends AppCompatActivity {
         });
 
         //Enlazamos con nuestro adaptador personalizado
-        MyAdapter myAdapter = new MyAdapter(this, R.layout.grid_item,names);
+         myAdapter = new MyAdapter(this, R.layout.grid_item,names);
       gridView.setAdapter(myAdapter);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu,menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.add_item:
+                this.names.add("Added nº"+(++counter));
+                this.myAdapter.notifyDataSetChanged();
+            return true;
+
+            default:
+            return super.onOptionsItemSelected(item);
+        }
 
 
     }
